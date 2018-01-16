76fc217592bb891e2502e9c500db858292304bb5
Guilherme Coppini Pavani <gpavani@uolinc.com>
2018-01-16 12:34:26 -0200

    Roda banco em memoria e cria base do Front

--- src/main/java/EstudandoJS/EstudandoJsApplication.java
+++ src/main/java/EstudandoJS/EstudandoJsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EstudandoJsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EstudandoJsApplication.class, args);
 	}
